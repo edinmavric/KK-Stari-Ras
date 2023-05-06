8a2aa8495b795c7507a266d5f4178cab0277f052
Edin Mavric <edinmavric10@gmail.com>
2023-05-06 20:58:38 +0200

    connected backend server with frontend

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,9 +1,21 @@
 import './Players.css';
+import { useEffect, useState } from 'react';
 
 const Players = () => {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.json())
+      .then(data => {
+        setBackendData(data)
+      });
+  }, []);
+
   return (
     <div className="Players">
       <h1>Players</h1>
+    {(typeof backendData.users === "undefined") ? (<p>Loading...</p>) : (backendData.users.map((user, key) => (<p key={key}>{user}</p>)))}
     </div>
   );
 };
