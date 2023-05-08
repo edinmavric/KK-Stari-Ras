a77e7cc920cfe3b96dd7b24ed19f1fe8a7846f92
Edin Mavric <edinmavric10@gmail.com>
2023-05-08 14:13:37 +0200

    displaying backend data

--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -2,20 +2,29 @@ import './Players.css';
 import { useEffect, useState } from 'react';
 
 const Players = () => {
-  const [backendData, setBackendData] = useState([{}]);
+  const [backendData, setBackendData] = useState([]);
 
   useEffect(() => {
     fetch('/api')
       .then(response => response.json())
       .then(data => {
-        setBackendData(data)
+        setBackendData(data);
       });
   }, []);
 
   return (
     <div className="Players">
       <h1>Players</h1>
-    {(typeof backendData.users === "undefined") ? (<p>Loading...</p>) : (backendData.users.map((user, key) => (<p key={key}>{user}</p>)))}
+      {typeof backendData.players === 'undefined' ? (
+        <p>Loading Players...</p>
+      ) : (
+        backendData.players.map((player, index) => <p key={index}>{player}</p>)
+      )}
+      {typeof backendData.points === 'undefined' ? (
+        <p>Loading Points...</p>
+      ) : (
+        backendData.points.map((points, index) => <p key={index}>{points}</p>)
+      )}
     </div>
   );
 };
