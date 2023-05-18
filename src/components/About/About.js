9dc34ce82ada9b7fd78264b06956d53bd0267e0a
Edin Mavric <edinmavric10@gmail.com>
2023-05-18 18:10:47 +0200

    overall style changes and added text to about me page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,172 @@ import './About.css';
 const About = () => {
   return (
     <div className="About">
-      <h1>About</h1>
+      <h1>ISTORIJA KLUBA</h1>
+      <div>
+        <strong>CILJEVI I ZADACI KLUBA</strong>
+        <p>
+          KK Stari Ras je osnovan 02.03.2011. Klub je nastao po ideji sportskih
+          akademija, u našoj zemlji, „FMP“, „Hemofarm“ i „Mladost“. Rad kluba
+          prvenstveno je koncentrisan na rad sa mlađim selekcijama, tj.
+          tehničko, intelektualno, pedagoško, unapređenje mladih kroz
+          kontrolisan, evidentiran i pravilan rad koji će obavljati stručnjaci.
+        </p>
+        <p>
+          Zadatak KK Stari Ras je da uči decu moralnim vrednostima, sportskom
+          duhu i takmičarskom duhu, podizanju intelekta na najviši mogući nivo,
+          jačanju svesti o zdravoj pravilnoj ishrani, o higijeni tela,
+          jednostavno korisnom, pre svega, pedagoškom radu sa decom.
+        </p>
+        <p>
+          Cilj KK Stari Ras je da realizuje dugoročni plan i program koji će
+          iznedriti tehnički i psihički sposobnog igrača za seniorski
+          tim.Izuzetnu podršku klubu u prvim godinama od osnivanja pružio je
+          Košarkaški savez Srbije , KK PARTIZAN-Beograd, KK SLOGA-Kraljevo,
+          Dejan Tomašević, Nenad Krstić, Dragan Kićanović i drugi.
+        </p>
+      </div>
+      <div>
+        <strong>SARADNJA SA DRUGIM KLUBOVIMA</strong>
+        <p>
+          U maju 2011. godine klub je zaključio saradnju i sporazum o međusobnom
+          razumevanju i pomoći sa KK PARTIZAN-Beograd. Tom prilikom je našem
+          tada novoformiranom klubu data puna podrška u nastojanju da Novi Pazar
+          postane košarkaška sredina i dogovoreno je učešće kadetske ekipe KK
+          PARTIZAN na našem turniru. SLOGA"-Kraljevo i uspostavlja bratske
+          odnose sa ovim klubom.
+        </p>
+        <p>
+          U avgustu 2020. godine nas klub je napravio veliki korak ka
+          unapredjivanju mladih i Novo Pazarske kosarke. Te prilike doslo je do
+          dogovora naseg kluba sa klubom legendarnog Pazarskog kosarkasa,
+          Fahrudina Djulovica. Nasa dva kluba su spojila mladje selekcije i
+          oformile mozda i najbolju kadetsku selekciju u istoriji Pazara
+          (Osvojivsi 2. mesto na nivou Medjuregionalne lige Srbije) i
+          oformljivanje seniorske selekcije koja pruza jake rezultate i igrace.
+        </p>
+      </div>
+      <div>
+        <strong>KOŠARKAŠKI TURNIRI KOD PETROVE CRKVE</strong>
+        <p>
+          Košarkaški klub Stari Ras je 2011. i 2012. godine organizovao dva
+          kadetska košarkaška turnira kod crkve Svetog Petra, koja su bila
+          izuzetno posećena, uz bogat kulturno-umetnički program.
+        </p>
+        <p>
+          Na prvom turniru, 2011. godine, učestvovale su četiri ekipe: KK
+          „Partizan“-Beograd, KK „Sloga“-Kraljevo, KK „Raška“ i KK „Stari Ras“.
+          Turnir je bio najposećenija sportska manifestacija u Novom Pazaru u
+          2011. godini (ne računajući fudbalske utakmice) jer je turnir posetilo
+          preko 4.000 ljudi za dva dana. Treba napomenuti da su gostovala neka
+          od najpoznatijih imena naše muzike, a to su Željko Vasić, Neverne
+          bebe, Beogradski sindikat. Turnir je zasluženo osvojila ekipa Sloge iz
+          Kraljeva.
+        </p>
+        <p>
+          Drugi turnir je organizovan 2012. godine pod pokroviteljstvom grada
+          Novog Pazara. Na turniru su učestvovale i četiri ekipe: KK
+          „Sloga“-Kraljevo, KK „Raška“, KK „Tutin“ i KK „Stari Ras“. Uz divnu
+          atmosferu i pred punim tribinama, turnir je osvojio pobjednik iz 2011.
+          godine, ekipa KK SLOGA, dok su drugo mesto osvojili kadeti našeg kluba
+          KK STARI RAS. Gosti turnira bili su: Marina Maljković (selektor ženske
+          reprezentacije), Željko Lukajić (trener) i Beogradski sindikat.
+        </p>
+      </div>
+      <div>
+        <strong>KOSARKASKI KAMPOVI U ORGANIZACIJI KLUBA</strong>
+        <p>
+          KK „Stari Ras“ od 2011. godine svakog leta organizuje košarkaški kamp.
+          Više od 600 učesnika iz našeg kluba, kao i iz drugih gradova i klubova
+          širom Srbije, odličan je pokazatelj kvalitetnog rada svih ovih godina.
+          Od 2017. godine Košarkaški klub "Stari Ras" zajedno sa još dva kluba
+          iz regiona organizuje veliki košarkaški kamp.
+        </p>
+      </div>
+      <div>
+        <strong>MLAĐE KATEGORIJE</strong>
+        <p>
+          Možda najprepoznatljiviji deo košarkaškog kluba "Stari Ras" su mlađe
+          kategorije. Akcenat je prvenstveno na profesionalnom radu, što našoj
+          velikoj košarkaškoj reprezentaciji daje odlične rezultate. U proteklih
+          nekoliko godina, nekoliko mladih igrača koji su potpisali ugovore sa
+          preko 250 djece, koliko trenutno ima naše članstvo, dovoljan su dokaz
+          o kakvoj je košarkaškoj organizaciji riječ.
+        </p>
+        <p>
+          Košarkaški klub "Stari Ras" takmiči se u regionalnim i kvalitetnim
+          ligama Košarkaškog saveza Raško Kosovsko Metohije u sledećim
+          kategorijama:
+        </p>
+        <p>
+          Minibasket 1, Minibasket 2, Mlađi pioniri 1, Mlađi pioniri 2, Pioniri,
+          Kadeti, Juniori.
+        </p>
+      </div>
+      <div>
+        <strong>SENIORI</strong>
+        <p>
+          Po prvi put u svojoj istoriji, klub u sezoni 2016/2017 osniva
+          seniorski tim, prvenstveno sastavljen od mladih talentovanih igrača
+          kako iz našeg objekta, tako i iz regiona. Prve godine su postigli
+          dobar rezultat zauzevši 5. mjesto, dok su sljedeće sezone imali nešto
+          slabiji rezultat, završivši takmičenje na 7. mjestu. Ove takmičarske
+          sezone 2018/2019, ekipa je vjerovatno najmlađa u cijeloj 2. MRL,
+          sastavljena prvenstveno od mladih igrača rođenih nakon 2002. godine.
+        </p>
+      </div>
+      <div>
+        <strong>ZENSKA SELEKCIJA</strong>
+        <p>
+          Košarkaški klub "Stari Ras" je 1. decembra 2014. godine osnovao
+          podružnicu svog kluba ŽKK Stari Ras u koju će se upisivati ​​samo
+          devojčice. Ova sekcija postiže odlične rezultate, gde su na školskim
+          takmičenjima na kojima su nastupili za OŠ „Meša Selimović“ bili prvi u
+          našem regionu i drugi u regionu centralne Srbije. Klubska sekcija
+          trenutno broji više od 30 djevojčica svih uzrasta.
+        </p>
+      </div>
+      <div>
+        <strong>NAVIJACI</strong>
+        <p>
+          Nešto po čemu ljudi prepoznaju naš klub su fanatični navijači, koji
+          prate i seniorsku i juniorsku ligu. Zna se da su mesto u sali OŠ "Meša
+          Selimović" popunjena sat vremena pre početka utakmica, pa gostujuće
+          ekipe s nelagodom dolaze u ovo malo košarkaško utočište. Kapacitet ove
+          školske sale je nešto veći od 300 mesta, što često zna biti premalo,
+          jer broj navijača i simpatizera višestruko premašuje tu cifru.
+        </p>
+      </div>
+      <div>
+        <strong>IGRACI U STRANIM KLUBOVIMA</strong>
+        <p>
+          Košarkaški klub "Stari Ras" izdvaja se od ostalih u N. Pazaru i ovom
+          delu Srbije po jedinstvenoj pripremi za slanje mladih i talentovanih
+          igrača u inostranstvo. Naime, naš klub je već poslao dva igrača u
+          Italiju, Tarika Hajrovića (2000. godište, 203 cm) i Dušana Božovića
+          (2001., 206 cm), koji sa velikim uspjehom nastupaju za tamošnje
+          klubove. Tarik je nastupio za "Latina Basket", a Dušan je potpisao za
+          "San Severo". Od čelnika našeg kluba saznajemo da će ove godine još
+          nekoliko igrača Starog Rasa naći zaposlenje u inostranim klubovima.
+        </p>
+        <p>
+          Jos jedna uspesna saradnja naseg kluba! Naime nasa dva mlada
+          talentovana igraca, Ersan Smailovic (2002. godiste, 200cm) i Veljko
+          Djurovic (2003. godiste, 200cm), posle dugih pregovora sa ekipom Sloge
+          iz Kraljeva potpisali su ugovore sa pomenutom ekipom. U nove pobede!
+        </p>
+        <p>
+          Lepe vesti stizu iz naseg kluba! Asim Djulovic (2005. godiste, 203cm)
+          i Ridvan Tutic (2002. godiste, 210cm) uspesno su potpisali ugovore u
+          jake klubove! Naime, nasa dva veoma talentovana igraca su uspesno
+          potpisali ugovore sa Megom iz Beograda (Asim Djulovic) i D1 ekipom
+          Wildcats-a iz Kentakija (Ridvan Tutic)! Asim je nevidjeni talenat i
+          radnik i sigurni smo da je lepa kosarkaska buducnost pred njim, sto i
+          rezultati koje postize u najelitnijim ligama Regiona i Evrope govore.
+          Ridvan je posle jako uspesne prve sezone u Americi proglasen za
+          Najboljeg mladog igraca (Rookie of the year) u njegovom delu Americkog
+          kontinenta! Svaka cast za ova dva momka!
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,13 +2,15 @@ import FacebookFeed from './FacebookFeed';
 import InstagramFeed from './InstagramFeed';
 import './News.css';
 import { AiFillInstagram, AiFillFacebook } from 'react-icons/ai';
+import bottomImage from './Images/Image-bottom-News.jpg';
+import topImage from './Images/Image-top-News.jpg';
 
 const News = () => {
   return (
     <div className="News">
-      <div className='News__header'>
-          <h1>Pratite najnovije vesti naseg kluba, sve na jednom mestu!</h1>
-        </div>
+      <div className="News__header">
+        <h1>Pratite najnovije vesti naseg kluba, sve na jednom mestu!</h1>
+      </div>
       <div className="News__Container">
         <div className="Instagram-News">
           <div className="News__timelines">
@@ -23,6 +25,10 @@ const News = () => {
             <h2>FACEBOOK</h2>
           </div>
           <FacebookFeed />
+        </div>
+        <div className="Image-News">
+          <img src={topImage} alt="" height="480" width='380' />
+          <img src={bottomImage} alt="" height="380" width='380' />
         </div>
       </div>
     </div>
